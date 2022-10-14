942d389a71343e851a1acae44bcf8f606ab4e003
Shane Wade <shanebramble96@hotmail.com>
2022-10-14 07:26:58 -0500

    created a submit event handler for the input form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandeler = function () {
+var createTaskHandeler = function (event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   taskToDoEl.appendChild(listItemEl);
+  
+  console.log(event);
 };
 
-buttonEl.addEventListener("click", createTaskHandeler);
+formEl.addEventListener("submit", createTaskHandeler);
